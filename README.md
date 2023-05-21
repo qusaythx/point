0f8be27d0f8da86cd36b5e7f40429453d9f484fb
qusaythx <134201037+qusaythx@users.noreply.github.com>
2023-05-21 13:04:59 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+
+### تابع : [QuSaY](https://t.me/F_C_A) ###
+
+![QuSaY](https://ibb.co/sVNvbsP)
---
+++ config.py
@@ -0,0 +1,78 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+import os
+APP_ID = os.environ.get("APP_ID")
+APP_HASH = os.environ.get("APP_HASH")
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
+session1 = os.environ.get("TERMUX")
+SESSION1 = os.environ.get("TERMUX")
+token = os.environ.get("TOKEN")
+DEVLOO = os.environ.get("DEVLO")
+
+qusay1 = TelegramClient(StringSession(session1), APP_ID, APP_HASH)
+bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
+
+
+session2 = os.environ.get("TERMUXX")
+SESSION2 = os.environ.get("TERMUXX")
+qusay2 = TelegramClient(StringSession(session2), APP_ID, APP_HASH)
+
+
+session3 = os.environ.get("TERMUXXX")
+SESSION3 = os.environ.get("TERMUXXX")
+qusay3 = TelegramClient(StringSession(session3), APP_ID, APP_HASH)
+
+
+session4 = os.environ.get("TERMUXXXX")
+SESSION4 = os.environ.get("TERMUXXXX")
+qusay4 = TelegramClient(StringSession(session4), APP_ID, APP_HASH)
+
+
+session5 = os.environ.get("TERMUXXXXX")
+SESSION5 = os.environ.get("TERMUXXXXX")
+qusay5 = TelegramClient(StringSession(session5), APP_ID, APP_HASH)
+
+session6 = os.environ.get("TERMUX6")
+SESSION6 = os.environ.get("TERMUX6")
+qusay6 = TelegramClient(StringSession(session6), APP_ID, APP_HASH)
+
+session7 = os.environ.get("TERMUX7")
+SESSION7 = os.environ.get("TERMUX7")
+qusay7 = TelegramClient(StringSession(session7), APP_ID, APP_HASH)
+
+session8 = os.environ.get("TERMUX8")
+SESSION8 = os.environ.get("TERMUX8")
+qusay8 = TelegramClient(StringSession(session8), APP_ID, APP_HASH)
+
+session9 = os.environ.get("TERMUX9")
+SESSION9 = os.environ.get("TERMUX9")
+qusay9 = TelegramClient(StringSession(session9), APP_ID, APP_HASH)
+
+session10 = os.environ.get("TERMUX10")
+SESSION10 = os.environ.get("TERMUX10")
+qusay10 = TelegramClient(StringSession(session10), APP_ID, APP_HASH)
+
+session11 = os.environ.get("TERMUX11")
+SESSION11 = os.environ.get("TERMUX11")
+qusay11 = TelegramClient(StringSession(session11), APP_ID, APP_HASH)
+
+session12 = os.environ.get("TERMUX12")
+SESSION12 = os.environ.get("TERMUX12")
+qusay12 = TelegramClient(StringSession(session12), APP_ID, APP_HASH)
+
+session13 = os.environ.get("TERMUX13")
+SESSION13 = os.environ.get("TERMUX13")
+qusay13 = TelegramClient(StringSession(session13), APP_ID, APP_HASH)
+
+session14 = os.environ.get("TERMUX14")
+SESSION14 = os.environ.get("TERMUX14")
+qusay14 = TelegramClient(StringSession(session14), APP_ID, APP_HASH)
+
+session15 = os.environ.get("TERMUX15")
+SESSION15 = os.environ.get("TERMUX15")
+qusay15 = TelegramClient(StringSession(session15), APP_ID, APP_HASH)
+
+ispay = ['yes']
+ispay2 = ['yes']
+bot.start()
+
